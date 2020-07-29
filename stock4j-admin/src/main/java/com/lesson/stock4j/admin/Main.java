ad89bcfc9ab312fbfad4f033439b66b19a02001d
lessoon <du.lee@foxmail.com>
2020-07-29 18:58:41 +0800

    1、修改mysql-connector版本 2、修改admin启动类

--- stock4j-admin/src/main/java/com/lesson/stock4j/admin/Main.java
+++ stock4j-admin/src/main/java/com/lesson/stock4j/admin/Main.java
@@ -1,11 +1,8 @@
-package com.lesson.stock4j.quartz;
+package com.lesson.stock4j.admin;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * 功能概述：启动类
